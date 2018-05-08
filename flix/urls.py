4622be2ae6bcffeeb55a00c3ed034feb630f3609
Lulu <lulumutuli24@gmail.com>
2018-05-08 16:14:41 +0300

    routing and views working

--- flix/urls.py
+++ flix/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.media_all,name='media_all'),
-    
+    url('^search/',views.media_search,name="media_search"),
+    url('^details/',views.media_details,name="media_details"),
+    url('^list/',views.media_rec,name="media_rec")
+
 ]
--- flix/views.py
+++ flix/views.py
@@ -5,3 +5,12 @@ from django.http  import HttpResponse
 
 def media_all(request):
     return render(request, 'all/all-media.html')
+
+def media_search(request):
+    return render(request, 'all/search.html')
+
+def media_details(request):
+    return render(request, 'all/details.html')
+
+def media_rec(request):
+    return render(request,'all/recc.html')
